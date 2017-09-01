ce6b634787b3a16cb938efb12c6fb89b15815084
RemyTombez <Remy.Tombez@formation.orif.ch>
2017-09-01 14:47:10 +0200

    Added new Classes : - Case - Ship - Grid - Player

---
+++ app/src/androidTest/java/Case.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ToRe on 01.09.2017.
+ */
+
+public class Case {
+    // Attributes
+    public boolean hited;
+    // TODO : Ship Attribute (EXACT data type have to be determined)
+
+    // Constructors
+    public Case(){
+        this.hited = false;
+        // TODO : Initialise Ship
+    }
+}
---
+++ app/src/androidTest/java/Ship.java
@@ -0,0 +1,17 @@
+/**
+ * Created by ToRe on 01.09.2017.
+ */
+
+public class Ship {
+    // Attributes
+    public int nbCases;
+    public String orientation;
+    public int nbHit;
+    // Constructors
+    Ship(){
+        this.nbCases = 0;
+        this.orientation = "up";
+        this.nbHit = 0;
+    }
+
+}
