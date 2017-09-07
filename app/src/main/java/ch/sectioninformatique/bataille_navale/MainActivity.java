4633b25fc771926acd81ddd472925db083b1ee09
LaurentSewer <Laurent.Sewer@formation.orif.ch>
2017-09-07 13:31:43 +0200

    Add : SettingActivity, 2 functional ReturnButton the CloseButton beginning interfaces and remove some warnings

--- app/src/main/java/ch/sectioninformatique/bataille_navale/MainActivity.java
+++ app/src/main/java/ch/sectioninformatique/bataille_navale/MainActivity.java
@@ -1,7 +1,10 @@
 package ch.sectioninformatique.bataille_navale;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button startGame = (Button) findViewById(R.id.playButton);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SetGameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button settingGame = (Button) findViewById(R.id.settingButton);
+        settingGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button closeApp = (Button) findViewById(R.id.closeButton);
+        closeApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/ch/sectioninformatique/bataille_navale/SettingActivity.java
+++ app/src/main/java/ch/sectioninformatique/bataille_navale/SettingActivity.java
@@ -1,19 +1,20 @@
 package ch.sectioninformatique.bataille_navale;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SetGameActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_game);
+        setContentView(R.layout.activity_setting);
 
-        Button returnButton2 = (Button) findViewById(R.id.ReturnButton);
-        returnButton2.setOnClickListener(new View.OnClickListener() {
+        Button returnButton = (Button) findViewById(R.id.ReturnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
