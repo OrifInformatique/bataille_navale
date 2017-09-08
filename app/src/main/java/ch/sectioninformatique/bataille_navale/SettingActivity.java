06c962c5c35c39236e4a6752b2374c1918d052e8
LaurentSewer <Laurent.Sewer@formation.orif.ch>
2017-09-08 12:40:15 +0200

    Add a Grid (no functional)

--- app/src/main/java/ch/sectioninformatique/bataille_navale/SettingActivity.java
+++ app/src/main/java/ch/sectioninformatique/bataille_navale/SettingActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class SettingActivity extends AppCompatActivity {
+    final Button randomColor = (Button) findViewById(R.id.ColorPlayer1Button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,22 @@ public class SettingActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        randomColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int A = 255;
+                int R = rand.nextInt(255);
+                int G = rand.nextInt(255);
+                int B = rand.nextInt(255);
+                int color = (A & 0xff) << 24 | (R & 0xff) << 16 | (G & 0xff) << 8 | (B & 0xff);
+
+
+
+            }
+        });
     }
+
+
 }
